ae9a903cc74a34f68eecc9ab2e95d05166229f9f
Adithep <badsxx@gmail.com>
2014-08-09 12:17:20 +0700

    new package

--- package.js
+++ package.js
@@ -1,14 +1,24 @@
 Package.describe({
-  summary: "Provides basic authentication"
+  summary: "Provides basic authentication",
+  version: "0.0.1"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['coffeescript', 'core-lib', 'utilities', 'alpha-stylus', 'standard-app-packages', 'accounts-base', 'accounts-password']);
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+  api.use([
+    'bads:core-lib',
+    'bads:utilities',
+    'bads:alpha-stylus',
+    'coffeescript',
+    'standard-app-packages',
+    'accounts-base',
+    'accounts-password'
+  ]);
   api.add_files(['authentication.html', 'authentication.coffee', 'authentication.styl'], 'client');
 });
 
 Package.on_test(function (api) {
-  api.use('alpha-auth');
+  api.use("../packages/bads:alpha-auth");
 
   api.add_files('authentication_tests.js', ['client', 'server']);
 });
